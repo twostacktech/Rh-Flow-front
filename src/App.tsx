cf9ce3a8bbfbb9b3e1f68735da2eb0c6cc1cb440
Juliana <juborges014@gmail.com>
2026-05-18 12:23:12 -0300

    feat: adicionado tailwind, router dom e axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './pages/home/Home'
 
 function App() {
 
   return (
-    <Home />
+    <>
+      <Home
+      />
+    </>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
-}
+return (
+<>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Teste
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white
+                                            border-white border-solid border-2 py-2 px-4'
+                            >
+                                Nova Postagem
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfrilv.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+ );
+ }
 
 export default Home
